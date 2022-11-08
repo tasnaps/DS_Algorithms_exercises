82b30896479441fb38be0eabeff64864f94c4178
Tapio Meriläinen <tapiom4@gmail.com>
2022-11-08 10:06:30 +0200

    Kolmas viikko

--- src/kolmasViikko/TRAII_22s_X3_pohja.java
+++ src/kolmasViikko/TRAII_22s_X3_pohja.java
@@ -26,30 +26,53 @@ public class TRAII_22s_X3_pohja implements TRAII_22s_X3 {
      */
     @Override
     public void containsNopeus(Collection<Double> C, SortedMap<Integer, Long> tulokset, boolean loytyy) {
-
+        //Etsittävä alkio pitää valita joko C löytyväksi jos loytyy, muuten ei loydy
+        double searchable = 0;
+        long startTime;
+        long taskTime = 0;
+        long parasTulos = 150000;
         // TODO ehkÃ¤ tÃ¤hÃ¤n
 
         // eri alkiomÃ¤Ã¤rÃ¤t joilla mittaus pitÃ¤Ã¤ tehdÃ¤:
         for (int alkioMaara : tulokset.keySet()) {
+            C.clear();//tyhjennetään aikaisemmat tulokset
+            C.addAll(satunnaisia(alkioMaara));//lisätään satunnaisia alkioita kokoelmaan
 
-            // TODO tÃ¤hÃ¤n ainakin pitÃ¤Ã¤ tehdÃ¤ jotain
 
-            // tulosten tallettaminen
-            tulokset.put(alkioMaara, 1L /* TODO tÃ¤hÃ¤n se tulos nanosekunteina */ );
+            if(!loytyy){
+                searchable = alkioMaara +1;
+                for(int i = 0; i<10; i++){
+                    startTime = System.nanoTime();
+                    C.contains(searchable);
+                    taskTime = System.nanoTime() - startTime;
+                    if((taskTime<parasTulos)&&taskTime!=0){
+                        parasTulos = taskTime;
+                    }
+                }
+
+            }
+
+            if(loytyy){
+                for(int i=0; i<10; i++){//kymmenen testiä
+                    //valitse alkio joka löytyy kokoelmasta.
+                    Iterator iterator = C.iterator();
+                    searchable = (double)iterator.next();
+                    startTime = System.nanoTime();
+                    C.contains(searchable);
+                    taskTime = System.nanoTime() - startTime;
+                    if((taskTime<parasTulos)&&taskTime!=0){
+                        parasTulos = taskTime;
+                    }
+                }
+
+            }
+            tulokset.put(alkioMaara, parasTulos);
 
         }
 
         // TODO ehkÃ¤ tÃ¤nnekin
 
     }
-
-
-    /**
-     * Luo ArrayList:n jossa jossa on n satunnaista liukulukua.
-     * Saa kÃ¤yttÃ¤Ã¤ jos haluaa.
-     * @param n liukulukujen mÃ¤Ã¤rÃ¤
-     * @return liukulukutaulukko
-     */
     private ArrayList<Double> satunnaisia(int n) {
         ArrayList<Double> A = new ArrayList<>(n);
         for (int i = 0; i < n; i++)
